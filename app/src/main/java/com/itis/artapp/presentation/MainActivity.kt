7bd880029a7a86c291e5dcc8bb8e7bb5b6eee6ed
AnastasiaChukhina <nas2002@list.ru>
2022-04-15 23:56:18 +0300

    user-permission added

--- app/src/main/java/com/itis/artapp/presentation/MainActivity.kt
+++ app/src/main/java/com/itis/artapp/presentation/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.itis.artapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.itis.artapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
